fb449cb3a1920bfd40fa3b4ab29bbb84c956424f
Pedro Queiroz <pedroqueiroz593@gmail.com>
2021-11-16 22:35:42 -0300

    feat: create docs (#4)
    * feat: create docs
    
    * chore: remove docs from eslint
    
    * chore: remove docs from linguist

---
+++ docs/pages/_app.tsx
@@ -0,0 +1,6 @@
+import 'nextra-theme-docs/style.css'
+import { AppProps } from 'next/app'
+
+export default function Nextra({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
