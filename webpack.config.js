13c5b90a24714534f453ced213bb71934b754942
Pedro Queiroz <pedroqueiroz593@gmail.com>
2021-11-16 21:58:40 -0300

    chore: setup linting (#2)

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 const path = require('path')
 
 module.exports = {
-  mode: 'production',
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.jsx'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"]
-        }
-      }
-    ]
-  }
+	mode: 'production',
+	entry: path.resolve(__dirname, 'dist', 'index.jsx'),
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'index.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env', '@babel/preset-react']
+				}
+			}
+		]
+	}
 }
