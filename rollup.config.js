3679e84fcd4e0dbd84f67cc03ec1e5e17eecf00c
pmqueiroz <pedroqueiroz593@gmail.com>
2021-11-17 15:22:28 -0300

    fix: remove reserved keyword

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,13 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
 
-import package from './package.json'
+import pkg from './package.json'
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: package.main,
+      file: pkg.main,
       format: 'cjs',
       exports: 'named',
       sourcemap: true,
