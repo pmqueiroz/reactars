081269ee0b312ee15ac2dd5313d0581dd6b45c7d
pmqueiroz <pedroqueiroz593@gmail.com>
2021-11-16 22:33:31 -0300

    feat: create docs

---
+++ docs/theme.config.js
@@ -0,0 +1,23 @@
+export default {
+   projectLink: 'https://github.com/pmqueiroz/reactar',
+   docsRepositoryBase: 'https://github.com/pmqueiroz/reactar/blob/master',
+   titleSuffix: ' – Reactar',
+   nextLinks: true,
+   prevLinks: true,
+   search: true,
+   customSearch: null,
+   darkMode: true,
+   footer: true,
+   footerText: `MIT ${new Date().getFullYear()} © pmqueiroz.`,
+   footerEditLink: `Edit this page on GitHub`,
+   logo: (
+      <h1>Reactar</h1>
+   ),
+   head: (
+     <>
+       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+       <meta name="description" content="Reactar: A high configurable react avatar component" />
+       <meta name="og:title" content="Reactar: A high configurable react avatar component" />
+     </>
+   ),
+ }
