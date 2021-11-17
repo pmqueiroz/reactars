da388466df85b6d381658e423596e2b659426456
pmqueiroz <pedroqueiroz593@gmail.com>
2021-11-17 15:27:37 -0300

    feat: config postcss

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
+import postcss from 'rollup-plugin-postcss'
 
 import pkg from './package.json'
 
@@ -20,7 +21,8 @@ export default {
    peerDepsExternal(),
    resolve(),
    commonjs(),
-   typescript({ useTsconfigDeclarationDir: true })
+   typescript({ useTsconfigDeclarationDir: true }),
+   postcss()
  ],
   external: ['react', 'react-dom'],
 }
