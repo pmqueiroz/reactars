29f61e7004c431bc15b313aa2aa772e1d7ac3f47
pmqueiroz <pedroqueiroz593@gmail.com>
2021-11-17 10:54:51 -0300

    feat: use avatar on demo

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,12 +1,12 @@
 import 'nextra-theme-docs/style.css'
 import { AppProps } from 'next/app'
 import { useEffect } from 'react'
-import { lastSha } from '../status.json'
+import status from '../status.json'
 
 export default function Nextra({ Component, pageProps }: AppProps) {
 
    useEffect(() => {
-      console.info(`%c Last SHA: ${lastSha}`, `color: purple; font-size: 2rem;`)
+      console.info(`%c Last SHA: ${status.lastSha}`, `color: purple; font-size: 2rem;`)
    })
 
   return <Component {...pageProps} />
