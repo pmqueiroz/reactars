0dc5d8ba727717906e91997939a36ae3ee387744
pmqueiroz <pedroqueiroz593@gmail.com>
2021-11-17 11:04:33 -0300

    feat: use avatar on demo

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -6,7 +6,7 @@ import status from '../status.json'
 export default function Nextra({ Component, pageProps }: AppProps) {
 
    useEffect(() => {
-      console.info(`%c Last SHA: ${status.lastSha}`, `color: purple; font-size: 2rem;`)
+      console.info(`%c Last SHA: ${status.lastSHA}`, `color: purple; font-size: 2rem;`)
    })
 
   return <Component {...pageProps} />
