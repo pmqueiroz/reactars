68705bea24f27484ff1826efca39677152980512
pmqueiroz <pedroqueiroz593@gmail.com>
2021-11-17 15:31:12 -0300

    test: jest handle scss files

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
    testEnvironment: 'jsdom',
    testPathIgnorePatterns: ['/node_modules/'],
    setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+   moduleNameMapper: {
+      "^.+\\.(css|less|scss)$": "babel-jest"
+   },
    globals: {
      'ts-jest': {
        isolatedModules: true,
