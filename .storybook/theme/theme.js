b04fe3430c8822b471c71e51948418ba6187183c
Pedro Queiroz <pedroqueiroz593@gmail.com>
2021-11-19 11:11:34 -0300

    fix: miss spell (#26)

--- .storybook/theme/theme.js
+++ .storybook/theme/theme.js
@@ -2,6 +2,6 @@ import { create } from '@storybook/theming/create'
 
 export default create({
    base: 'light',
-   brandTitle: 'Reactra',
-   brandUrl: 'https://github.com/pmqueiroz/reactra'
+   brandTitle: 'Reactar',
+   brandUrl: 'https://github.com/pmqueiroz/reactar'
 })
