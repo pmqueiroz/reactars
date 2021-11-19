34758c99213efde8a549d49f6514b7a6b0e48d89
pmqueiroz <pedroqueiroz593@gmail.com>
2021-11-18 22:03:27 -0300

    fix: badge color fallbacks

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { mergeDeepRight } from 'ramda'
 
 import { Wrapper } from './avatar.styles'
-import { AllowedBadgeTypes, DefaultBadgeType, AvatarProps, BadgeColor } from './interface'
+import { AllowedBadgeTypes, DefaultBadgeType, AvatarProps, AvatarStylesOwnProps } from './interface'
 
 const allowedBadgeTypesMap: AllowedBadgeTypes[] = ['none', 'notification', 'status']
 
@@ -14,32 +15,32 @@ export const Avatar = <BageType extends AllowedBadgeTypes = DefaultBadgeType>(pr
 
    const letter = props.name?.charAt(0) || '?'
 
-   const badgeColor: BadgeColor = {
-      notification: '#E65A4D',
-      busy: '#E65A4D',
-      idle: '#F7C656',
-      offline: '#CCCCCC',
-      online: '#50CA42'
-   }
-
    const overlapProps = {
       count: (props as unknown as AvatarProps<'notification'>).count > 99 ? 99 : (props as unknown as AvatarProps<'notification'>).count,
    }
-
-   const stylesProps = {
+   
+   const defaultProps = {
       size: 48,
       backgroundColor: '#2A2C2C',
       radii: 'circle',
       badgePosition: 'top-right',
       badgeType,
-      badgeColor,
       status: 'online',
+      name: 'Reactra',
+      letter,
       notificationTextColor: '#FFFFFF',
-      ...restProps,
-      ...overlapProps
-   }
+      badgeColors: {
+         notification: '#E65A4D',
+         busy: '#E65A4D',
+         idle: '#F7C656',
+         offline: '#CCCCCC',
+         online: '#50CA42'
+      }
+   } as AvatarStylesOwnProps
+
+   const stylesProps = mergeDeepRight(mergeDeepRight(defaultProps, restProps), overlapProps) as unknown as AvatarStylesOwnProps
 
    return (
-      <Wrapper data-testid="reactra" letter={letter} {...stylesProps} />
+      <Wrapper data-testid="reactra" {...stylesProps} />
    )
 }
